d7c2d4948acc62bac0941614592f9c98ddd1b781
LAPTOP-7NO6ANAA\navee <rakeshsenapathi@gmail.com>
2021-10-24 20:26:43 +0530

    First draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { AiFillGithub , AiFillLinkedin, AiFillMediumSquare} from 'react-icons/ai';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header>
+        <img className = "header-image" src="" alt = "header logo"></img>
+      </header> */}
+      <div className="container-first">
+      <section className="section-logo">
+        <h1 className= "logo-name"><span className = "logo-para">Hey I'm </span><br/>Rakesh</h1>
+        <div className="logo">
+          <div className = "logo-image"></div>
+        </div>
+        <h1 className="logo-para--1">Software Engineer</h1>
+        <h1 className="logo-para--2">Degen | Memer | Sports/E-sports enthusiast</h1>
+      </section>
+      <section className="socials">
+        <div className = "socials-icons">
+          <span className = "scaleOnHover marginLeftRight glowbg-icons"><AiFillGithub size = "3.5rem"/></span>
+          <span className = "scaleOnHover marginLeftRight glowbg-icons" ><AiFillLinkedin size = "3.5rem"/></span>
+          <span className = "scaleOnHover marginLeftRight glowbg-icons"><AiFillMediumSquare size = "3.5rem"/></span>
+        </div>
+      </section>
+      <button className="connectBtn">
+        Connect
+      </button>
+      </div>
+      <div className="container-last">
+      <section className="attributes">
+        <div className="attributes-container"></div>
+      </section>
+      </div>
     </div>
   );
 }
